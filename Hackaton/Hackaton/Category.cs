8c9f6cbe46937d8d6e4b0b19acbc60c6eea281a2
zexydreamer <kedr2800@icloud.com>
2019-05-18 13:27:45 +0500

    Add: Category.cs

---
+++ Hackaton/Hackaton/Category.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Drawing;
+
+namespace Hackaton
+{
+    public class Category
+    {
+        public Bitmap Image;
+        public string Name;
+        public List<Product> ListItems;
+
+    }
+}
